a45b21e3f502c428679fb14a7a47e5b202b21d76
devdanti <143756582+devdanti@users.noreply.github.com>
2023-10-11 17:23:25 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+let img = document.querySelector('.container__img');
+
+function clicar() {
+    if (img.style.width = "40px" && img.style.height = "40px")
+        img.style.width = "60px" && img.style.height = "60px"
+}
